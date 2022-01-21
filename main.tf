0b9bff48f13f39ba5169ff70b5a350eb66e0ae47
Yagorus <yegor.sidenko@gmail.com>
2022-01-21 16:11:38 +0200

    test

--- main.tf
+++ main.tf
@@ -6,6 +6,12 @@ module "subnet" {
     source      = "./modules/subnet"
     vpc_id      = module.vpc.vpc_id
     environment  = module.vpc.environment
-    aws_region = var.aws_region
-    aws_profile = var.aws_profile   
+    #aws_region = var.aws_region
+    #aws_profile = var.aws_profile   
+}
+
+module "security" {
+    source = "./modules/security"
+    vpc_id  = module.vpc.vpc_id
+    environment = module.vpc.environment
 }
