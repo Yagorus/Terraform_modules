52c76bcd8ece653e857a3491ab0697d0f474afa2
Yagorus <yegor.sidenko@gmail.com>
2022-01-21 14:54:55 +0200

    test

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ module "vpc" {
 
 module "subnet" {
     source      = "./modules/subnet"
-    vpc_id      = "${module.vpc.vpc_id}"
+    vpc_id      = module.vpc.vpc_id
     environment  = module.vpc.environment
     #aws_profile_access_key_path = var.aws_profile_access_key_path
     aws_region = var.aws_region
