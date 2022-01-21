1c2c345dce34d86702683cef45526d9562537015
Yagorus <yegor.sidenko@gmail.com>
2022-01-21 14:49:31 +0200

    Test

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ module "subnet" {
     source      = "./modules/subnet"
     vpc_id      = module.vpc.vpc_id
     environment  = module.vpc.environment
-    aws_profile_access_key_path = var.aws_profile_access_key_path
+    #aws_profile_access_key_path = var.aws_profile_access_key_path
     aws_region = var.aws_region
     aws_profile = var.aws_profile   
 }
--- terraform.tf
+++ terraform.tf
@@ -1,7 +1,7 @@
 provider "aws" {
     region = var.aws_region
     profile = var.aws_profile
-    shared_credentials_file = var.aws_profile_access_key_path
+    #shared_credentials_file = var.aws_profile_access_key_path
 }
 
 terraform {
