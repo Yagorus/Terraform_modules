b39d4ed0cff356216e9f00077b5cd7f5ccac7e34
Yagorus <yegor.sidenko@gmail.com>
2022-01-21 18:37:48 +0200

    asd

--- modules/ec2/user_data.sh
+++ modules/ec2/user_data.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-yum -y update 
-yum -y install httpd
+sudo yum -y update 
+sudo yum -y install httpd
 myip=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
-echo "<h2>WebServer with IP: $myip</h2> build by Terraform" > /var/www/html/index.html
+echo "<h2>WebServer with IP: $myip build by Terraform</h2> " > /var/www/html/index.html
 sudo service httpd start
 chkconfig httpd on
