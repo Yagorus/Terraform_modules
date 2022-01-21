5722ecaf6dba44e8d0d9bbde7e2178b6a812e358
Yagorus <yegor.sidenko@gmail.com>
2022-01-21 19:01:12 +0200

    asdf

--- modules/subnet/main.tf
+++ modules/subnet/main.tf
@@ -29,7 +29,7 @@ resource "aws_route_table" "PublicRT" {
   vpc_id = var.vpc_id.id
 
   route  {
-    cidr_block  = "10.0.0.0/16"
+    cidr_block  = "0.0.0.0/16"
     gateway_id   = aws_internet_gateway.InternetGateway.id
   }
 
